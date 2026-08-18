01fd169d9f87bb327931b7ed4d274ce5725b8fd2
Solid22-cpu <warrensmith951@gmail.com>
2026-08-17 20:38:34 -0500

    index.html

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'solid22-cache-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+// Install: pre-cache core files
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Activate: clean up any old cache versions
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: network-first for HTML/JS so visitors always get the latest version.
+// Falls back to cache only if the network request fails (i.e. offline).
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
